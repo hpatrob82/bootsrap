6a23db598fa87a4be125233b99b1e0c30cf6253f
hpatrob82 <75027266+hpatrob82@users.noreply.github.com>
2021-01-16 16:55:32 -0500

    small_bootstrap
    added js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 'use script';
+
+
+function updateBody(quote) {
+    const paragraph = document.querySelector('div #modal-body p');
+    paragraph.innerHTML = quote;
+
+}
+
+
+
+
+const form = document.querySelector('#changeQuote');
+const categorySelect = document.createElement('select'); {
+    const categoryOption = document.createElement('option');
+
+
+};
+
+
+categorySelect.addEventListener('change', function(event) {
+    getQuote(event.target.value);
+})
